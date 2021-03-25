cf7af35bea4de28b7da01a46a67d1a40c7521173
HEMMER-FX <hemmerfrancoisxavier@gmail.com>
2021-03-25 14:02:06 +0100

    derniere modif pour le texte

--- pages/histoire.php
+++ pages/histoire.php
@@ -55,20 +55,21 @@
 
             <div class="contenu_main">
                 <h2>Lycée</h2>
-                <p> Alors là par où commencer ? Mon premier jour au lycée fût magnifique, après avoir passé quelques mois loin de ma chérie "Élie Coptère" ( mais elle ne savait pas que je l'aimais au collège) je l'ai enfin revue, elle a fichtrement bien changée. <br>
-                    je savais pas que des nénés pouvaient pousser si vite (c'est peut être comme les légumes, mon père mets des pesticides il dis que sa repousse les bactéries et que ça donne un meilleur goût.)
-                    C'est au lycée que j'ai rencontré mon meilleur ami, "Clint Horris" il a grandi aux USA mais ses parents sont venus habiter en France, dès que je lui ai parlé on a tout de suite accroché ensemble, il est juste plus agé que moi car il a 31 ans, tout le monde
+                <p> Alors là par où commencer ? <span class="souligne"> Mon premier jour </span> au lycée fût <span class="important">magnifique </span>, après avoir passé quelques mois loin de ma chérie <span class="important">"Élie Coptère" </span>( mais elle ne savait pas que je l'aimais au collège) je l'ai enfin revue, elle a fichtrement bien changée. <br>
+                    je savais pas que des <span class="souligne">nénés</span> pouvaient pousser si vite (c'est peut être comme les légumes, mon père mets des pesticides il dis que sa repousse les bactéries et que ça donne un meilleur goût.)
+                    C'est au lycée que j'ai rencontré mon meilleur ami, <span class="important">"Clint Horris"</span> il a grandi aux USA mais ses parents sont venus habiter en France, dès que je lui ai parlé on a tout de suite accroché ensemble, il est juste plus agé que moi car il a <span class="important">31 ans</span>, tout le monde
                     se moquait de lui à cause de son âge mais les gens ne savent pas pourquoi il est au collège, c'est parce qu'il veut connaitre tous ses cours par coeur avant de partir du lycée, moi je trouve que c'est un malin.</p>
             </div>
 
             <div class="contenu_main">
                 <h2>Vie professionnelle</h2>
-                <p> Je suis sorti plus tôt que mon ami Clint Horris car je n'ai pas la patience d'apprendre tous mes cours, j'aime charbonner et bouger mes doigts. A ma sortie de lycée j'avais 22 ans (et encore toutes mes dents ) je dis ça car Clint Horris lui il les perds
-                    il me dit que c'est à cause du stress des cours mais j'ai un doute je pense plutôt  que c'est parce qu'il se brosse les dents avec des cigarettes qu'il a fumé. En tout cas voilà dès ma sortie de lycée je suis allé travailler chez Roland Culé, c'est un agriculteur
-                    qui vit a côté de la maison, il fait pousser des plantes médicinales qui se vendent très chères, il a beaucoup de clients et moi j'ai beaucoup à apprendre de lui. Après quelques années passées à ses côtés je me dois de changer de métier car il y a des jours
+                <p> Je suis sorti <span class="important">plus tôt </span>que mon ami Clint Horris car je n'ai pas la patience d'apprendre tous mes cours, j'aime charbonner et bouger mes doigts. A ma sortie de lycée j'avais <span class="important">22 ans </span>(et encore toutes mes dents ) je dis ça car Clint Horris lui il les perds
+                    il me dit que c'est à cause du stress des cours mais j'ai un doute je pense plutôt  que c'est parce qu'il se brosse les dents avec des cigarettes qu'il a fumé. En tout cas voilà dès ma sortie de lycée je suis allé travailler chez <span class="important">Roland Culé </span>, c'est un agriculteur
+                    qui vit a côté de la maison, il fait pousser des plantes médicinales qui se vendent <span class="important">très chères</span>, il a beaucoup de clients et moi j'ai beaucoup à apprendre de lui. Après quelques années passées à ses côtés je me dois de changer de métier car il y a des jours
                     je crois que je ne supporte plus ce que Roland Culé fait, il brûle les mauvaises herbes mais moi après j'ai souvent envie de dormir. D'après mes parents c'est parce que le métier ne m'intéresse plus assez. C'est dommage je l'aimais bien mon patron et ce qu'il
-                    fallait faire. Je pense que je vais me diriger vers une nouvelle voie, j'aime mon village et je pense que je vais me lancer dans la propreté du village, je commencerai par faire les trottoirs, comme ça je dirais à toutes les personnes que je croiserai d'arrêter
-                    de jeter leurs chewing gum avec le goût bizarre, j'en ai une fois ramassé un pour le goûter c'était pas bon , il avait un goût très âpre et le jus très salé.</p>
+                    fallait faire. Je pense que je vais me diriger vers une nouvelle voie, j'aime mon village et je pense que je vais me lancer dans la propreté du village, je commencerai par faire les <span class="important">trottoirs</span>, comme ça je dirais à toutes les personnes que je croiserai d'arrêter
+                    de jeter leurs chewing gum avec le goût bizarre, j'en ai une fois ramassé un pour le goûter c'était pas bon , il avait un goût très âpre et le jus très salé. En plus ce sera peut être l'occasion de faire des rencontres car dans mon village
+                    il y a beaucoup de personnes qui <span class="important"> attendent </span> près du trottoir.</p>
             </div>
 
         </main>
